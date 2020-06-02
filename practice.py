53a48dfaf7d1a6b41e9b8c0a8b195833d083c1b2
Chanelle <ckaith97@yahoo.com>
2020-06-01 23:24:06 -0700

    createDictionary() is an easy too. to create a bag of word about a topic

--- practice.py
+++ practice.py
@@ -44,50 +44,74 @@ def synonymExercise2(words):
 
 ##
 from nltk.corpus import wordnet
-def foodDictionary():
+def createDictionary():
     path = "logs/foods.txt"
     f = open(path, "a+")
     root = "food"
 
-    queue = [root]
-
+    queue = []
     # Keep track of the words I've already included and don't include again
     history = []
+    syns = wordnet.synsets(root)
+    for syn in syns:
+        queue.append(syn)
+
     while len(queue) > 0:
-        word = queue.pop()
+        syn = queue.pop(0)
 
         # Sometimes duplicates are in the queue, skip them if we already did it
-        if word in history:
+        if syn in history:
             continue
-
         # Otherwise this is a new word, save to history
-        history.append(word)
-        syns = wordnet.synsets(word)
+        history.append(syn)
 
-        # Save the current word
-        f.write(syns[0].lemmas()[0].name() + ",")
+        # Lemmas can help describe what the word is
+        # Decide whether you want to include these lemmas
+        lems = [lem for lem in syn.lemmas()]
+        if len(lems) > 0:
+            print(lems, ": ", syn.definition())
+            print("Do you want to include these into the food log? (y if yes)")
+            user = input()
+            # If 'y' then write into file
+            if user == 'y':
+                # Save the lemmas into the file
+                for lem in lems:
+                    if lem not in history:
+                        f.write(lem.name() + ",")
 
-        for syn in syns:
-            # Extract all other lemmas
-            lems = [lem.name() for lem in syn.lemmas()[1:]]
+        # Decide whether you want to include these hypos
+        hypos = syn.hyponyms()
+        if len(hypos) > 0:
+            print("hypos: ")
 
-            # Decide whether you want to include these lemmas
-            if len(lems) > 0:
-                print(lems, ": ", syn.definition())
-                print("Do you want to include these into the food log? (y if yes)")
-                log_this = input()
+            # Add all hyponyms for this word to queue
+            for idx in range(len(hypos)):
+                print(idx, ": ", hypos[idx].name(), ": ", hypos[idx].definition())
 
-                # If 'y' then write into file
-                if log_this == 'y':
-                    for lem in lems:
-                        if lem not in history:
-                            f.write(lem + ",")
-                            queue.append(lem)
+            print("Do you want to include these into the food log and queue?")
+            print("Enter the ones you want to include as a comma delimited list: <1, 2, 6> OR enter 'all' for all")
+            user = input()
 
-                # If exit, close the file and stop this func
-                if log_this == "exit!":
-                    f.close()
-                    return
+            # If all, keep them all
+            if user == "all":
+                for hypo in hypos:
+                    if hypo not in history:
+                        f.write(hypo.lemmas()[0].name() + ",")
+                        queue.append(hypo)
+            # Otherwise, get the indices of the hypos the user specified to keep and keep them
+            ids = user.split(',')
+            for id in ids:
+                if not id.isnumeric() or int(id) > len(hypos):
+                    continue
+                hypo = hypos[int(id)]
+                if hypo not in history:
+                    f.write(hypo.name() + ",")
+                    queue.append(hypo)
+
+            # If exit, close the file and stop this func
+            if user == "exit!":
+                f.close()
+                return
 
 
 
